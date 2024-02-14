ffa0871058b27932bfe11d286357897a89402e25
beresB <57649310+beresB@users.noreply.github.com>
2024-02-14 23:09:34 +0100

    added data models

---
+++ WSH_HomeAssignment.Infrastructure/Data/Models/ExchangeRateRecord.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSH_HomeAssignment.Domain.Entities;
+
+namespace WSH_HomeAssignment.Infrastructure.Data.Models
+{
+    public class ExchangeRateRecord
+    {
+        public DateTime Date { get; set; }
+        public string Currency { get; set; } = null!;
+        public int Unit { get; set; }
+        public double Value { get; set; }
+        public ExchangeRate ToExchangeRate()
+        {
+            return new ExchangeRate(Currency, Unit, Value);
+        }
+    }
+    public class ExchangeRateRecordConfiguration : IEntityTypeConfiguration<ExchangeRateRecord>
+    {
+        public void Configure(EntityTypeBuilder<ExchangeRateRecord> builder)
+        {
+            builder.HasKey(r => new { r.Date,r.Currency});
+            builder.Property(r => r.Currency).HasMaxLength(DomainConstants.CurrencyMaxLength);
+        }
+    }
+}
---
+++ WSH_HomeAssignment.Infrastructure/Data/Models/SavedExchangeRateRecord.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System.ComponentModel.DataAnnotations.Schema;
+using WSH_HomeAssignment.Domain.Entities;
+
+namespace WSH_HomeAssignment.Infrastructure.Data.Models
+{
+    [PrimaryKey(nameof(UserId),nameof(Date),nameof(Currency))]
+    public class SavedExchangeRateRecord
+    {
+        public string UserId { get; set; } = null!;
+        public string Currency { get; set; } = null!;
+        public DateTime Date { get; set; }
+        public string? Note { get; set; }
+
+        public virtual ExchangeRateRecord ExchangeRate { get; set; } = null!;
+        public virtual IdentityUser User { get; set; } = null!;
+    }
+    public class SavedExchangeRateRecordConfiguration : IEntityTypeConfiguration<SavedExchangeRateRecord>
+    {
+        public void Configure(EntityTypeBuilder<SavedExchangeRateRecord> builder)
+        {
+            builder.HasKey(r => new { r.Date, r.Currency,r.UserId });
+            builder.Property(r => r.Currency).HasMaxLength(DomainConstants.CurrencyMaxLength);
+            builder.Property(r => r.Note).HasMaxLength(DomainConstants.NoteMaxLength);
+
+            builder.HasOne(nameof(SavedExchangeRateRecord.ExchangeRate))
+                   .WithMany()
+                   .HasForeignKey(nameof(SavedExchangeRateRecord.Date), nameof(SavedExchangeRateRecord.Currency));
+
+            builder.HasOne(nameof(SavedExchangeRateRecord.User))
+                   .WithMany()
+                   .HasForeignKey(nameof(SavedExchangeRateRecord.UserId));
+        }
+    }
+}
