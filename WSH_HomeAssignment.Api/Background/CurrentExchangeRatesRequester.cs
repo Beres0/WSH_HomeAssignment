f1e0ed667b7504b78a2039a3cdff427df8a2fe95
beresB <57649310+beresB@users.noreply.github.com>
2024-02-14 23:11:19 +0100

    added current exchange rates requester background service

---
+++ WSH_HomeAssignment.Api/Background/CurrentExchangeRatesRequester.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using WSH_HomeAssignment.Domain.Exceptions;
+using WSH_HomeAssignment.Domain.ExchangeServices;
+using WSH_HomeAssignment.Domain.Repositories;
+
+namespace WSH_HomeAssignment.Api.Background
+{
+    public class CurrentExchangeRatesRequester : BackgroundService
+    {
+        private readonly IServiceScopeFactory serviceScopeFactory;
+
+        public CurrentExchangeRatesRequester(IServiceScopeFactory serviceScopeFactory)
+        {
+            this.serviceScopeFactory = serviceScopeFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            do
+            {
+                using (var scope = serviceScopeFactory.CreateScope())
+                {
+                    var service=scope.ServiceProvider.GetRequiredService<ICachedExchangeRatesService>();
+                    await service.RefreshAsync(stoppingToken);
+                }
+
+                await Task.Delay(TimeSpan.FromHours(6),stoppingToken);
+            } while (!stoppingToken.IsCancellationRequested);
+        }
+    }
+}
