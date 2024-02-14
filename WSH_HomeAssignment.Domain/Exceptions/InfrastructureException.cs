65f2174f3bffe0c457bcbf655508349efbc6cf13
beresB <57649310+beresB@users.noreply.github.com>
2024-02-14 23:00:48 +0100

    exceptions added

---
+++ WSH_HomeAssignment.Domain/Exceptions/InfrastructureException.cs
@@ -0,0 +1,9 @@
+﻿namespace WSH_HomeAssignment.Domain.Exceptions
+{
+    public class InfrastructureException:Exception
+    {
+        public InfrastructureException(Exception? innerException,string? message=null):base(message,innerException)
+        {
+        }
+    }
+}
