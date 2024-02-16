1950da944f23c77b7b9469879e1e9b6d12359155
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:22:30 +0100

    replaced DateTime with DateOnly

--- WSH_HomeAssignment.Infrastructure/ExchangeRatesServices/MNBExchangeRatesService/DailyExchangeRatesXmlParser.cs
+++ WSH_HomeAssignment.Infrastructure/ExchangeRatesServices/MNBExchangeRatesService/DailyExchangeRatesXmlParser.cs
@@ -6,29 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using WSH_HomeAssignment.Domain.Entities;
+using WSH_HomeAssignment.Domain.Entities.WSH_HomeAssignment.Domain.Entities;
 
 namespace WSH_HomeAssignment.Infrastructure.ExchangeRatesService
 {
     public class DailyExchangeRatesXmlParser
     {
-        public virtual DailyExchangeRates Parse(string xml)
+        public virtual DailyExchangeRateCollection Parse(string xml)
         {
             XDocument doc = XDocument.Parse(xml);
             var dateString = doc.Descendants("Day").First().Attribute("date")!.Value;
-            var date=DateTime.ParseExact(dateString,
+            var date=DateOnly.ParseExact(dateString,
                                          "yyyy-MM-dd",
                                          CultureInfo.CurrentCulture);
 
-            var exchangeRates = new Dictionary<string,ExchangeRate>();
-            foreach (var rateNode in doc.Descendants("Rate"))
+            var exchangeRates = doc.Descendants("Rate").Select(r =>
             {
-                var currency = rateNode.Attribute("curr")!.Value;
-                var unit =int.Parse(rateNode.Attribute("unit")!.Value);
-                var value=double.Parse(rateNode.Value);
-                exchangeRates.Add(currency, new ExchangeRate(currency, unit, value));
-            }
-
-            return new DailyExchangeRates(date, exchangeRates);
+                var currency = r.Attribute("curr")!.Value;
+                var unit = int.Parse(r.Attribute("unit")!.Value);
+                var value = double.Parse(r.Value);
+                return new ExchangeRate(currency, unit, value);
+            });
+            return new DailyExchangeRateCollection(date, exchangeRates);
         }
     }
 }
