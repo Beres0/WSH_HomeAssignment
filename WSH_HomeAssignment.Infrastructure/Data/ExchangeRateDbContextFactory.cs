cfab58a7a5c7598f9ab8b134d49dcab069212e83
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:45:16 +0100

    added DbContextFactory

---
+++ WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContextFactory.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+namespace WSH_HomeAssignment.Infrastructure.Data
+{
+    public class ExchangeRateDbContextFactory : IDesignTimeDbContextFactory<ExchangeRateDbContext>
+    {
+        public ExchangeRateDbContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<ExchangeRateDbContext>();
+            builder.UseSqlServer("Server=(LocalDb)\\\\MSSQLLocalDB;Database=ExchangeRates;Trusted_Connection=True;TrustServerCertificate=True");
+
+            return new ExchangeRateDbContext(builder.Options);
+        }
+    }
+}
