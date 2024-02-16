38a19923903cf905e2ef17d8eed97302c13b63ac
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:37:12 +0100

    SavedExchangeRate contains ExchangeRate

--- WSH_HomeAssignment.Domain/Entities/SavedExchangeRate.cs
+++ WSH_HomeAssignment.Domain/Entities/SavedExchangeRate.cs
@@ -1,38 +1,29 @@
-﻿using WSH_HomeAssignment.Domain.Exceptions;
-
-namespace WSH_HomeAssignment.Domain.Entities
+﻿namespace WSH_HomeAssignment.Domain.Entities
 {
     public class SavedExchangeRate:IEntity
     {
-        public DateTime Date { get; }
-        public string Currency { get; }
-        public int Unit { get; }
-        public double Value { get; }
+        public string UserId { get; }
+        public DailyExchangeRate ExchangeRate { get; }
         public string? Note { get; private set; } 
 
-        public SavedExchangeRate(DateTime date, string currency, int unit, double value)
+        public SavedExchangeRate(DailyExchangeRate exchangeRate,string userId)
         {
-            InvalidEntityException.CheckMin(date, DomainConstants.DateMin);
-            InvalidEntityException.CheckNullOrWhiteSpace(currency);
-            InvalidEntityException.CheckMaxLength(currency, DomainConstants.CurrencyMaxLength);
-            InvalidEntityException.CheckMin(unit, DomainConstants.ValueMin);
-            InvalidEntityException.CheckMin(value, DomainConstants.UnitMin);
-
-            Date = date;
-            Currency = currency;
-            Unit = unit;
-            Value = value;
+            InvalidArgumentException.CheckNullOrWhiteSpace(userId);
+            InvalidArgumentException.CheckNull(exchangeRate);
+            UserId = userId;
+            ExchangeRate = exchangeRate;
         }
+
         public SavedExchangeRate SetNote(string? note)
         {
-            InvalidEntityException.CheckMaxLength(note, 100);
+            InvalidArgumentException.CheckMaxLength(note, 100);
             Note = note;
             return this;
         }
 
         public object[] GetKey()
         {
-            return new object[] { Date, Currency };
+            return new object[] { ExchangeRate.Date, ExchangeRate.Currency, UserId };
         }
 
       
