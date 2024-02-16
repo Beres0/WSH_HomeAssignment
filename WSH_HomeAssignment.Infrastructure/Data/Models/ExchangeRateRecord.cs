3d98ff02e800e5c5081caf3bfeff0cadc5e464fd
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:29:39 +0100

    switch to internal from public

--- WSH_HomeAssignment.Infrastructure/Data/Models/ExchangeRateRecord.cs
+++ WSH_HomeAssignment.Infrastructure/Data/Models/ExchangeRateRecord.cs
@@ -5,28 +5,26 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using WSH_HomeAssignment.Domain.Entities;
 
 namespace WSH_HomeAssignment.Infrastructure.Data.Models
 {
-    public class ExchangeRateRecord
+    internal class ExchangeRateRecord
     {
         public DateTime Date { get; set; }
         public string Currency { get; set; } = null!;
         public int Unit { get; set; }
         public double Value { get; set; }
-        public ExchangeRate ToExchangeRate()
-        {
-            return new ExchangeRate(Currency, Unit, Value);
-        }
+   
     }
-    public class ExchangeRateRecordConfiguration : IEntityTypeConfiguration<ExchangeRateRecord>
+    internal class ExchangeRateRecordConfiguration : IEntityTypeConfiguration<ExchangeRateRecord>
     {
         public void Configure(EntityTypeBuilder<ExchangeRateRecord> builder)
         {
-            builder.HasKey(r => new { r.Date,r.Currency});
+            builder.HasKey(nameof(ExchangeRateRecord.Date),nameof(ExchangeRateRecord.Currency));
             builder.Property(r => r.Currency).HasMaxLength(DomainConstants.CurrencyMaxLength);
         }
     }
