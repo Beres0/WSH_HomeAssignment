844af3112446771bc0e69321bf59c774475e78e5
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:37:37 +0100

    removed strong typed IEntity

--- WSH_HomeAssignment.Domain/Entities/IEntity.cs
+++ WSH_HomeAssignment.Domain/Entities/IEntity.cs
@@ -1,9 +1,5 @@
 ﻿namespace WSH_HomeAssignment.Domain.Entities
 {
-    public interface IEntity<TKey>:IEntity
-    {
-        TKey Id { get; }
-    }
     public interface IEntity 
     {
         public object[] GetKey();
