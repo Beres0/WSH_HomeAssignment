7d77322df645c82b74f64a1eb4ee617294bf6dfb
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:34:42 +0100

    replaced DateTime

--- WSH_HomeAssignment.Domain/Entities/DomainConstants.cs
+++ WSH_HomeAssignment.Domain/Entities/DomainConstants.cs
@@ -9,10 +9,11 @@ namespace WSH_HomeAssignment.Domain.Entities
 
     public static class DomainConstants
     {
-        public static DateTime DateMin => new DateTime(1900, 1, 1);
+        public static DateOnly DateMin => new DateOnly(1900, 1, 1);
         public const int NoteMaxLength= 100;
         public const int CurrencyMaxLength = 10;
         public const int UnitMin = 1;
         public const double ValueMin = 0;
+        public const int PasswordRequiredLength = 6;
     }
 }
