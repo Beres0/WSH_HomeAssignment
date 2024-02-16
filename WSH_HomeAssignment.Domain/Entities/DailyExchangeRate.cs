600e1cf6482930455c2e45abdaa764ee9c6ef316
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:33:22 +0100

    added DailyExchangeRate

---
+++ WSH_HomeAssignment.Domain/Entities/DailyExchangeRate.cs
@@ -0,0 +1,18 @@
+﻿namespace WSH_HomeAssignment.Domain.Entities
+{
+
+    public class DailyExchangeRate:ExchangeRate
+    {
+        public DailyExchangeRate(DateOnly date,string currency, int unit, double value) : base(currency, unit, value)
+        {
+            InvalidArgumentException.CheckMin(date, DomainConstants.DateMin);
+        }
+
+        public DateOnly Date { get; }
+        public override object[] GetKey()
+        {
+            return new object[] {Date, Currency };
+        }
+    }
+
+}
