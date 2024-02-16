12bb1b37648e16ebc9832899fc75a86c5879e791
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:43:50 +0100

    applied database configurations

--- WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContext.cs
+++ WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContext.cs
@@ -8,17 +8,27 @@ using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using WSH_HomeAssignment.Infrastructure.Data.Models;
 using Microsoft.AspNetCore.Identity;
 using WSH_HomeAssignment.Domain.Entities;
+using System.Reflection;
 
 namespace WSH_HomeAssignment.Infrastructure.Data
 {
+
     public class ExchangeRateDbContext:IdentityDbContext<IdentityUser>
     {
-        public ExchangeRateDbContext(DbContextOptions options) : base(options)
+        public ExchangeRateDbContext(DbContextOptions<ExchangeRateDbContext> options) : base(options)
         {
         }
 
-        public virtual DbSet<ExchangeRateRecord> ExchangeRates { get; set; } = null!;
-        public virtual DbSet<SavedExchangeRateRecord> SavedExchangeRates { get; set; } = null!;
-        
+        internal virtual DbSet<ExchangeRateRecord> ExchangeRates { get; set; } = null!;
+        internal virtual DbSet<SavedExchangeRateRecord> SavedExchangeRates { get; set; } = null!;
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
