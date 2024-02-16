e16b7b17c3d76dee739962cf8cd3137d8d4920b6
Benjámin Béres <beresb0@gmail.com>
2024-02-16 16:49:50 +0100

    added User entity

---
+++ WSH_HomeAssignment.Domain/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿namespace WSH_HomeAssignment.Domain.Entities
+{
+    public class User:IEntity
+    {
+        public User(string id,string userName,string email)
+        {
+            InvalidArgumentException.CheckNullOrWhiteSpace(id);
+            InvalidArgumentException.CheckNullOrWhiteSpace(userName);
+            InvalidArgumentException.CheckNullOrWhiteSpace(email);
+            Id = id;
+            UserName = userName;
+            Email = email;
+        }
+        public string Id { get; }
+        public string UserName { get;}
+        public string Email { get; }
+        public object[] GetKey()
+        {
+            return new object[] { Id };
+        }
+    }
+
+}
