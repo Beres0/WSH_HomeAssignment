0be0f5154c3278de0c161b4c9700048bf9903499
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:40:28 +0100

    removed configuring

--- WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContext.cs
+++ WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContext.cs
@@ -21,10 +21,6 @@ namespace WSH_HomeAssignment.Infrastructure.Data
 
         internal virtual DbSet<ExchangeRateRecord> ExchangeRates { get; set; } = null!;
         internal virtual DbSet<SavedExchangeRateRecord> SavedExchangeRates { get; set; } = null!;
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            base.OnConfiguring(optionsBuilder);
-        }
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
