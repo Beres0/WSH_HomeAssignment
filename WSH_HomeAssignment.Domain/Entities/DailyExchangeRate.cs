501007b64c5ae0d36a384f80a4a48ed14ebfa78d
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:43:56 +0100

    fixed date init

--- WSH_HomeAssignment.Domain/Entities/DailyExchangeRate.cs
+++ WSH_HomeAssignment.Domain/Entities/DailyExchangeRate.cs
@@ -6,6 +6,8 @@
         public DailyExchangeRate(DateOnly date,string currency, int unit, double value) : base(currency, unit, value)
         {
             InvalidArgumentException.CheckMin(date, DomainConstants.DateMin);
+
+            Date = date;
         }
 
         public DateOnly Date { get; }
