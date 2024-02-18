4bb6c5a5a54fdc037fbdb9169d83569284614302
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:45:15 +0100

    checked key in indexer

--- WSH_HomeAssignment.Domain/Entities/DailyExchangeRateCollection.cs
+++ WSH_HomeAssignment.Domain/Entities/DailyExchangeRateCollection.cs
@@ -1,65 +1,63 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using WSH_HomeAssignment.Domain.Repositories;
+using System.Collections;
 namespace WSH_HomeAssignment.Domain.Entities
 {
-    using System;
-    using System.Collections;
-    using System.Collections.Generic;
-    using System.Diagnostics.CodeAnalysis;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    namespace WSH_HomeAssignment.Domain.Entities
+    public class DailyExchangeRateCollection : IEntity, IReadOnlyDictionary<string, ExchangeRate>
     {
-        public class DailyExchangeRateCollection : IEntity, IReadOnlyDictionary<string, ExchangeRate>
+        public DateOnly Date { get; }
+
+        public IEnumerable<string> Keys => exchangeRates.Keys;
+
+        public IEnumerable<ExchangeRate> Values => exchangeRates.Values;
+
+        public int Count => exchangeRates.Count;
+
+        public ExchangeRate this[string currency]
         {
-            public DateOnly Date { get; }
-
-            public IEnumerable<string> Keys => exchangeRates.Keys;
-
-            public IEnumerable<ExchangeRate> Values => exchangeRates.Values;
-
-            public int Count => exchangeRates.Count;
-
-            public ExchangeRate this[string key] => exchangeRates[key];
-            private readonly IReadOnlyDictionary<string, ExchangeRate> exchangeRates;
-            public DailyExchangeRateCollection(DateOnly date, IEnumerable<ExchangeRate> exchangeRates)
+            get
             {
-                InvalidArgumentException.CheckMin(date, DomainConstants.DateMin);
+                if (!exchangeRates.TryGetValue(currency, out ExchangeRate? value))
+                {
+                    EntityNotFoundException.Check<ExchangeRate>(value, Date, currency);
+                }
+                return value!;
+            }
+        }
+        private readonly IReadOnlyDictionary<string, ExchangeRate> exchangeRates;
+        public DailyExchangeRateCollection(DateOnly date, IEnumerable<ExchangeRate> exchangeRates)
+        {
+            InvalidArgumentException.CheckMin(date, DomainConstants.DateMin);
 
-                 Date = date;
-                this.exchangeRates = exchangeRates.ToDictionary(r => r.Currency, r => r);
-            }
-            public bool ContainsKey(string key)
-            {
-                return exchangeRates.ContainsKey(key);
-            }
+            Date = date;
+            this.exchangeRates = exchangeRates.ToDictionary(r => r.Currency, r => r);
+        }
+        public bool ContainsKey(string currency)
+        {
+            return exchangeRates.ContainsKey(currency);
+        }
 
-            public bool TryGetValue(string key, [MaybeNullWhen(false)] out ExchangeRate value)
-            {
-                return exchangeRates.TryGetValue(key, out value);
-            }
+        public bool TryGetValue(string currency, [MaybeNullWhen(false)] out ExchangeRate value)
+        {
+            return exchangeRates.TryGetValue(currency, out value);
+        }
 
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return GetEnumerator();
-            }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
-            public object[] GetKey()
-            {
-                return new object[] { Date };
-            }
+        public virtual object[] GetKey()
+        {
+            return new object[] { Date };
+        }
 
-            public IEnumerator<KeyValuePair<string, ExchangeRate>> GetEnumerator()
-            {
-                return exchangeRates.GetEnumerator();
-            }
+        public IEnumerator<KeyValuePair<string, ExchangeRate>> GetEnumerator()
+        {
+            return exchangeRates.GetEnumerator();
         }
     }
 }
