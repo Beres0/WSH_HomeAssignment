160812d720fe652c554dbfb099384dc9203814ec
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:57:26 +0100

    configured routes

--- WSH_HomeAssignment.Client/src/app/app-routing.module.ts
+++ WSH_HomeAssignment.Client/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { mapToCanActivate, RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth-guard/auth.guard';
+import { UnAuthGuard } from './auth-guard/un-auth.guard';
+import { CurrentExchangeRateComponent } from './pages/current-exchange-rate/current-exchange-rate.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
+import { SavedExchangeRateListComponent } from './pages/saved-exchange-rate-list/saved-exchange-rate-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: CurrentExchangeRateComponent,
+    canActivate: mapToCanActivate([AuthGuard]),
+  },
+  {
+    path: 'saved',
+    component: SavedExchangeRateListComponent,
+    canActivate: mapToCanActivate([AuthGuard]),
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: mapToCanActivate([UnAuthGuard]),
+  },
+  {
+    path: 'registration',
+    component: RegistrationComponent,
+    canActivate: mapToCanActivate([UnAuthGuard]),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
