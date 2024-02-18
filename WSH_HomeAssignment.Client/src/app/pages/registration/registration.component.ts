36b617343a91a9cdd639749a33ab2decce8b3069
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:55:45 +0100

    added registration page

---
+++ WSH_HomeAssignment.Client/src/app/pages/registration/registration.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/api-proxy-services/auth/auth.service';
+import { ErrorDto, RegisterDto } from 'src/app/api-proxy-services/models/types';
+
+@Component({
+  selector: 'app-registration',
+  templateUrl: './registration.component.html',
+  styleUrls: ['./registration.component.scss']
+})
+export class RegistrationComponent {
+  errorMsg?:string[]
+  registerModel:RegisterDto={userName:"",email:"",password:""}
+  submitting:boolean=false
+  confirmedPassword:string=""
+  constructor(private authService:AuthService,private router:Router){
+  }
+  submitDisabled(){
+    return this.submitting||this.confirmedPassword!=this.registerModel.password
+  }
+
+  async onSubmit(){
+    try{
+      this.submitting=true
+      await this.authService.register(this.registerModel);
+      this.router.navigate(["/"])
+    }catch(error){
+      this.errorMsg=ErrorDto.CreateFromError(error)?.message
+    }
+    finally{
+      this.submitting=false
+    }
+  }
+}
