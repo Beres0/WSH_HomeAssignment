7cc7ecd569539f65683ee966c95e526fb67aac16
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:58:27 +0100

    app component completed

--- WSH_HomeAssignment.Client/src/app/app.component.ts
+++ WSH_HomeAssignment.Client/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { AuthService } from './api-proxy-services/auth/auth.service';
+import { ExchangeRatesService } from './api-proxy-services/exchange-rates/exchange-rates.service';
+import { ValidationStoreService } from './api-proxy-services/validation/validation-store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'WSH_HomeAssignment.Client';
+  constructor(private authService:AuthService,private validationService:ValidationStoreService){
+  }
+  async ngOnInit(){
+      await this.validationService.getValues()
+  }
+  isLoggedIn(){
+    return this.authService.isLoggedIn()
+  }
 }
