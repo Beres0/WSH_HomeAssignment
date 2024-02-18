9b9a2aa6023b03d64ca8b174374ddf359a464a7e
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:46:37 +0100

    added ValidationDto

---
+++ WSH_HomeAssignment.Api/Services/Validation/ValidationDto.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WSH_HomeAssignment.Api.Services.ExchangeRates;
+using WSH_HomeAssignment.Domain.Entities;
+
+namespace WSH_HomeAssignment.Api.Services.Validation
+{
+ 
+    public class ValidationDto
+    {
+        public int NoteMaxLength { get; set; } = DomainConstants.NoteMaxLength;
+        public int UnitMin { get; set; } = DomainConstants.UnitMin;
+        public double ValueMin { get; set; } = DomainConstants.ValueMin;
+        public int PasswordRequiredLength { get; set; } = DomainConstants.PasswordRequiredLength;
+        public DateDto DateMin { get; set; } = new DateDto();
+        public int CurrencyMaxLength { get; set; }= DomainConstants.CurrencyMaxLength;
+    }
+}
