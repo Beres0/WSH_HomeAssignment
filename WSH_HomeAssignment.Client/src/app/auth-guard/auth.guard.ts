6cbcb664c035fb17f041b884f7a7a49420dcdc09
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:54:48 +0100

    added auth guards

---
+++ WSH_HomeAssignment.Client/src/app/auth-guard/auth.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../api-proxy-services/auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard{
+
+  constructor(private authService:AuthService,private router: Router) { 
+  }
+  canActivate(){
+    if(this.authService.isLoggedIn()){
+      return true
+    }
+    else{
+      this.router.navigate(["/login"])
+    }
+    return false;
+  }
+}
