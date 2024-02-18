315a9b97a907f71c313ae59d0ab3f7e13de77bc9
beresB <57649310+beresB@users.noreply.github.com>
2024-02-18 16:43:19 +0100

    removed

--- WSH_HomeAssignment.Domain/Infrastructure/InfrastructureException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WSH_HomeAssignment.Domain.Infrastructure
-{
-    public class InfrastructureException:Exception
-    {
-        public InfrastructureException(Exception? innerException,string? message=null):base(message,innerException)
-        {
-        }
-    }
-}
