8164286f3dafd11b99473773e157317ef8a5fd7e
beresB <57649310+beresB@users.noreply.github.com>
2024-02-19 19:09:36 +0100

    fixed connstring

--- WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContextFactory.cs
+++ WSH_HomeAssignment.Infrastructure/Data/ExchangeRateDbContextFactory.cs
@@ -1,13 +1,22 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
 namespace WSH_HomeAssignment.Infrastructure.Data
 {
     public class ExchangeRateDbContextFactory : IDesignTimeDbContextFactory<ExchangeRateDbContext>
     {
+        private string? ConnString { get; set; }
+
+        public ExchangeRateDbContextFactory(IConfiguration configuration)
+        {
+            ConnString = configuration.GetConnectionString("DefaultConnection");
+        }
+
         public ExchangeRateDbContext CreateDbContext(string[] args)
         {
             var builder = new DbContextOptionsBuilder<ExchangeRateDbContext>();
-            builder.UseSqlServer("Data Source=(localdb)\\mssqllocaldb;Database=exchange-rates;Trusted_Connection=True;MultipleActiveResultSets=true");
+            builder.UseSqlServer(ConnString);
 
             return new ExchangeRateDbContext(builder.Options);
         }
