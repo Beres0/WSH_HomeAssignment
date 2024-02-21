4510f45e2aaf73e0f283943ad51ea815b61df6ad
Benjámin Béres <beresb0@gmail.com>
2024-02-21 17:38:06 +0100

    revert

--- WSH_HomeAssignment.Client/src/app/api-proxy-services/auth/auth.service.ts
+++ WSH_HomeAssignment.Client/src/app/api-proxy-services/auth/auth.service.ts
@@ -44,19 +44,21 @@ export class AuthService extends ApiService {
     }
 
     getToken() {
-        const stored = localStorage.getItem(this.tokenKey);
-        if (stored == null) {
-            this.logout();
-            return undefined;
+        if (this.cachedToken == undefined) {
+            const stored = localStorage.getItem(this.tokenKey);
+            if (stored == null) {
+                return undefined;
+            }
+            const token = Object.assign({}, JSON.parse(stored));
+            this.cachedToken = token;
         }
+
         const now = new Date().getTime();
         const expiration = new Date(this.cachedToken!.expiration).getTime();
         if (now >= expiration) {
             this.logout();
             return undefined;
         }
-        const token = Object.assign({}, JSON.parse(stored));
-        this.cachedToken = token;
         return this.cachedToken;
     }
 }
